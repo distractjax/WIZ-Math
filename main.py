e7adb46eb09aff397f44a7e91f0d6334c31e2507
distractjax <distractjax@gmail.com>
2026-01-13 17:20:57 -0500

    Trying to fix

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 from fractions import multiply_by_fractions
 from fractions import divide_by_fractions
+from basics import function_picker
+
 
 def main():
-    print(multiply_by_fractions())
-    print(divide_by_fractions())
+    input1 = input('What would you like to do? Type "Get Options" if you want to see all the available functions, or just select "Random" to get started.')
+    function_picker(input1)
 
 if __name__ == "__main__":
     main()
