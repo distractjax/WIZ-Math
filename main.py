e6ab13b98abc51ac5563ec9a6f8653f83208aec9
distractjax <distractjax@gmail.com>
2026-01-13 16:55:14 -0500

    Added functions for multiplying and dividing fractions

--- main.py
+++ main.py
@@ -3,6 +3,31 @@
 from math import sqrt, ceil
 from functools import reduce
 from array import array, ArrayType
+from random import randrange, randint
+
+def divide_by_fractions() -> str:
+    '''
+    Generates a string that divides two fractions.
+    '''
+    solution_numerator = randint(1, 100)
+    solution_denominator = randint(1, 100)
+
+    numerator_factors = find_factors(solution_numerator)
+    denominator_factors = find_factors(solution_denominator)
+
+    numerator1, denominator2 = multiply_by_result(solution_numerator)
+    denominator1, numerator2 = multiply_by_result(solution_denominator)
+
+    while True:
+        for factor in numerator_factors:
+            if factor in denominator_factors and factor != 1:
+                solution_numerator = solution_numerator // factor
+                solution_denominator = solution_denominator // factor
+        break
+
+    result = input(f'What is the result of ({numerator1}/{denominator1}) / ({numerator2}/{denominator2})?\n')
+    result = ''.join([ch for ch in result if not ch.isspace()])
+    return 'Correct' if result == f'{solution_numerator}/{solution_denominator}' else f'Incorrect, the solution is:\n{solution_numerator} / {solution_denominator}'
 
 def find_factors(input: int) -> array[int]:
     '''
@@ -13,18 +38,42 @@ def find_factors(input: int) -> array[int]:
     output = array('I',sorted(output))
     return output
 
-def divide_by_fractions(input: int) -> str:
+def multiply_by_fractions() -> str:
     '''
-    Generates a string that divides two fractions made of up a given number's factors.
+    Generates a string that multiplies two fractions.
+    '''
+    solution_numerator = randint(1, 100)
+    solution_denominator = randint(1, 100)
+
+    numerator_factors = find_factors(solution_numerator)
+    denominator_factors = find_factors(solution_denominator)
+
+    numerator1, numerator2 = multiply_by_result(solution_numerator)
+    denominator1, denominator2 = multiply_by_result(solution_denominator)
+
+    while True:
+        for factor in numerator_factors:
+            if factor in denominator_factors and factor != 1:
+                solution_numerator = solution_numerator // factor
+                solution_denominator = solution_denominator // factor
+        break
+
+    result = input(f'What is the result of ({numerator1}/{denominator1}) * ({numerator2}/{denominator2})?\n')
+    result = ''.join([ch for ch in result if not ch.isspace()])
+    return 'Correct' if result == f'{solution_numerator}/{solution_denominator}' else f'Incorrect, the solution is:\n{solution_numerator} / {solution_denominator}'
+
+def multiply_by_result(input: int) -> int:
+    '''
+    Returns two numbers that multiply to create the result.
     '''
     factors = find_factors(input)
-
+    index_int = randrange(len(factors) // 2)
+    factor1, factor2 = factors[index_int], factors[-index_int - 1]
+    return factor1, factor2
 
 def main():
-    print("Hooray!")
-    print(find_factors(10))
-    factors = find_factors(10)
-    print(factors[3])
+    print(multiply_by_fractions())
+    print(divide_by_fractions())
 
 if __name__ == "__main__":
     main()
