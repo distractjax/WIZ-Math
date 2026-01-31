b89ebb1ff137407f3bb6b3a01a0f2c7bb86f6794
distractjax <distractjax@gmail.com>
2026-01-31 01:09:00 -0500

    Got started on the vertical border as well.

--- tui.py
+++ tui.py
@@ -1,28 +1,42 @@
 from shutil import get_terminal_size 
 
-def outline():
+def draw_outline():
     width, height = get_terminal_size()
-    print(f'Width: {width}\nHeight: {height}')
-    print(get_terminal_size())
-    for x in range (1, height // 2):
-        print("|")
-    print(draw_horizontal_border(width))
-    return "outline"
 
-def draw_horizontal_border(width: int) -> str:
-    '''
-    This function returns a string that draws a horizontal border given the width of a screen.
-    '''
-    padding = width // 2
-    padding = padding // 2
+    width_padding = width // 4
+    width = width - width_padding
 
-    width = width - padding * 2
+    height = height // 2
+
+    height_padding = height // 4
+    height = height - height_padding
+
+    draw_horizontal_border(width, width_padding)
+    draw_vertical_border(height, width, width_padding)
+    draw_horizontal_border(width, width_padding)
+
+def draw_horizontal_border(width: int, padding: int, height_padding: int = 2) -> str:
+    '''
+    This function returns a string that draws a horizontal border given the width of a screen and a horizontal padding.
+    '''
+    height_padding_str = ''.join(["\n" for x in range(0, height_padding // 2)])
 
     padding_str = ''.join([" " for x in range(0, padding)])
 
     middle_str = ''.join(["_" for x in range (0, width)])
 
-    return padding_str + middle_str + padding_str
+    print(height_padding_str + padding_str + middle_str + padding_str + height_padding_str)
+
+def draw_vertical_border(height: int, width: int, width_padding: int) -> str:
+    '''
+    This function returns a string that draws a vertical border given the height of a screen and both horizontal and vertical padding.
+    '''
+    width_str = ''.join(" " for x in range(0, width))
+
+    width_str[width_padding] = "|"
+    width_str[-width_padding] = "|"
+
+    print('\n'.join([width_str for x in range(0, height)]))
 
 if __name__ == "__main__":
-    outline()
+    draw_outline()
