e3e32dbf73442e2b3ffeea5d6b1b322997c4d74a
distractjax <distractjax@gmail.com>
2026-02-03 14:53:50 -0500

    Added some tests to determine where the centering function was going wrong.

--- tui.py
+++ tui.py
@@ -1,16 +1,16 @@
 from shutil import get_terminal_size 
 
+def test_width():
+    width, height = get_terminal_size()
+    test_string = ''.join("1" for x in range(0, width))
+    print(test_string)
+
 def draw_outline():
     width, height = get_terminal_size()
-
     width_padding = width // 4
-    width = width - width_padding
-
-    height = height // 2
-
+    width = width - (width_padding * 2)
     height_padding = height // 4
-    height = height - height_padding
-
+    height = height - (height_padding * 2)
     draw_horizontal_border(width, width_padding)
     draw_vertical_border(height, width, width_padding)
     draw_horizontal_border(width, width_padding)
@@ -20,23 +20,31 @@ def draw_horizontal_border(width: int, padding: int, height_padding: int = 2) ->
     This function returns a string that draws a horizontal border given the width of a screen and a horizontal padding.
     '''
     height_padding_str = ''.join(["\n" for x in range(0, height_padding // 2)])
-
-    padding_str = ''.join([" " for x in range(0, padding)])
-
-    middle_str = ''.join(["_" for x in range (0, width)])
-
-    print(height_padding_str + padding_str + middle_str + padding_str + height_padding_str)
+    padding_str = ''.join(["1" for x in range(0, padding)])
+    middle_str = ''.join(["0" for x in range (0, width)])
+    print(padding_str + middle_str + padding_str)
 
 def draw_vertical_border(height: int, width: int, width_padding: int) -> str:
     '''
     This function returns a string that draws a vertical border given the height of a screen and both horizontal and vertical padding.
     '''
-    width_str = ''.join(" " for x in range(0, width))
-
-    width_str[width_padding] = "|"
-    width_str[-width_padding] = "|"
-
+    width_array = [" " for x in range(0, width)]
+    width_array[width_padding] = "|"
+    width_array[-width_padding-1] = "|"
+    width_str = ''.join(x for x in width_array)
     print('\n'.join([width_str for x in range(0, height)]))
+
+def test_draw() -> str:
+    '''
+    This is a test function that sees how things are working
+    '''
+    width_array = [str(x) for x in range(0, 5)]
+    width_array[1] = "|"
+    width_array[-2] = "|"
+    width_str = ''.join(x for x in width_array)
+    print('\n'.join([width_str for x in range(0, 10)]))
 
 if __name__ == "__main__":
     draw_outline()
+    # test_draw()
+    test_width()
