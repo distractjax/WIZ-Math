90edbbe3cf4da4a8d3d692e12c084b34e5c53c34
distractjax <distractjax@gmail.com>
2026-02-07 00:36:44 -0500

    Got the window switching working.

--- tui.py
+++ tui.py
@@ -11,46 +11,73 @@ def enter_is_terminate(x):
     return x
 
 def tui_event_loop(stdscr):
-    stdscr = curses.initscr()
-    curses.start_color()
     curses.use_default_colors()
-    curses.noecho()
-    curses.cbreak()
     stdscr.keypad(True)
     curses.curs_set(2)
     stdscr.clear()
     header = 'Testing zone'
     footer = "Type 'Quit' to exit"
+
+    sub1_cols = (curses.COLS - 6) // 5
+    remain_cols = curses.COLS - 6 - sub1_cols
+
+    sub1 = stdscr.subwin(curses.LINES - 3, sub1_cols, 1, 2)
+
+    sub2_lines = 3 * (curses.LINES - 5) // 4
+    sub2 = stdscr.subwin(sub2_lines,remain_cols + 3, 1, sub1_cols + 2)
+
+    sub3_lines = curses.LINES - 5 - sub2_lines
+    sub3 = stdscr.subwin(sub3_lines,remain_cols - 2, sub2_lines + 2, sub1_cols + 3)
+
+    windows = [sub1, sub2, sub3]
+    for i, w in enumerate(windows):
+        w.keypad(True)
+        w.leaveok(False)
+
+    active_idx = 0
+
     while True:
         stdscr.addstr(0,2,header,curses.A_BOLD)
         stdscr.addstr(curses.LINES-2,2,footer,curses.A_BOLD)
-        sub1_cols = (curses.COLS - 6) // 5
-        remain_cols = curses.COLS - 6 - sub1_cols
-        sub1 = stdscr.subwin(curses.LINES - 3, sub1_cols, 1, 2)
+
         sub1.border()
-        sub2_lines = 3 * (curses.LINES - 5) // 4
-        sub3_lines = curses.LINES - 5 - sub2_lines
-        sub2 = stdscr.subwin(sub2_lines,remain_cols + 3, 1, sub1_cols + 2)
-        sub3 = stdscr.subwin(sub3_lines,remain_cols - 2, sub2_lines + 2, sub1_cols + 3)
         sub2.border()
+
         rectangle(stdscr,sub2_lines + 1, sub1_cols + 2, curses.LINES - 3, curses.COLS - 2)
-        tb = Textbox(sub3)
-        stdscr.refresh()
-        stdscr.move(2,3)
-        c = stdscr.getch()
+
+        stdscr.noutrefresh()
+        for w in windows:
+            w.noutrefresh()
+
+        target_win = windows[active_idx]
+        target_win.move(1, 1)
+
+        curses.doupdate()
+
+        c = target_win.getch()
+
         if c == curses.KEY_RIGHT:
-            sub2.move(1,1)
+            active_idx = (active_idx + 1) % len(windows)
+
+        elif c == curses.KEY_LEFT:
+            active_idx = (active_idx - 1) % len(windows)
+
         elif c == curses.KEY_DOWN:
+            active_idx = 2
             sub3.move(0,0)
+
+            tb = Textbox(sub3)
             tb.edit(enter_is_terminate)
             message = tb.gather().strip()
+
             if message.lower() == 'quit':
                 break
-            else:
-                sub2.addstr(1,1,f'{message}')
-                sub2.clrtoeol()
+
+            sub2.addstr(1,1,f'{message}')
+            sub2.clrtoeol()
             sub3.clear()
-        else:
+
+        elif c == ord('q'):
             break
 
 curses.wrapper(tui_event_loop)
