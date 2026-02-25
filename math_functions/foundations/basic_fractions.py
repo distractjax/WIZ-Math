670d59b4f685181e98af71516ecb31e9901243dd
distractjax <distractjax@gmail.com>
2026-02-25 01:08:42 -0500

    Added in math_functions from main.

---
+++ math_functions/foundations/basic_fractions.py
@@ -0,0 +1,100 @@
+from random import randint
+from math_functions.common_operations import simplify_fractions
+import config
+
+# Fractions
+
+def divide_fractions_quiz() -> bool:
+    '''
+    Generates a string that divides two fractions.
+    '''
+    numerator1 = randint(1,20)
+    numerator2 = randint(1,20)
+
+    denominator1 = randint(2,20)
+    denominator2 = randint(2,20)
+
+    while denominator1 == numerator1:
+        denominator1 = randint(2, 20)
+    while denominator2 == numerator2:
+        denominator2 = randint(2, 20)
+
+    solution_numerator = numerator1 * denominator2
+    solution_denominator = denominator1 * numerator2
+
+    simplified_solution = simplify_fractions(solution_numerator,solution_denominator)
+
+    question = f'What is the result of ({numerator1}/{denominator1}) / ({numerator2}/{denominator2})?'
+    answer = f'{simplified_solution[0]}/{simplified_solution[1]}'
+
+    config.write_solution(question,answer)
+
+def multiply_fractions_quiz() -> bool:
+    '''
+    Generates a string that multiplies two fractions.
+    '''
+    numerator1 = randint(1,20)
+    numerator2 = randint(1,20)
+
+    denominator1 = randint(2,20)
+    denominator2 = randint(2,20)
+
+    while denominator1 == numerator1:
+        denominator1 = randint(2, 20)
+    while denominator2 == numerator2:
+        denominator2 = randint(2, 20)
+
+    solution_numerator = numerator1 * numerator2
+    solution_denominator = denominator1 * denominator2
+
+    simplified_solution = simplify_fractions(solution_numerator,solution_denominator)
+
+    question = f'What is the result of ({numerator1}/{denominator1}) * ({numerator2}/{denominator2})?'
+    answer = f'{simplified_solution[0]}/{simplified_solution[1]}'
+
+    config.write_solution(question,answer)
+
+# TODO: Add in division function for this same process
+def multiply_fractions_with_exponents() -> bool:
+    '''
+    Generates a string that multiplies two fractions that are defined by exponents.
+    '''
+    numerator1 = randint(2,10)
+    numerator2 = randint(2,10)
+    denominator = randint(2,10)
+
+    while denominator == numerator1 or denominator == numerator2:
+        denominator = randint(2, 20)
+
+    numerator1_exponent = randint(2,5)
+    numerator2_exponent = randint(2,5)
+    denominator_exponent = randint(2,5)
+
+    solution_numerator = (numerator1 ** numerator1_exponent) * (numerator2 ** numerator2_exponent)
+    solution_denominator = denominator ** denominator_exponent
+
+    simplified_solution = simplify_fractions(solution_numerator,solution_denominator)
+
+    question = f'What is the result of ({numerator1}^{numerator1_exponent}) * ({numerator2}^{numerator2_exponent}) / ({denominator}^{denominator_exponent})?'
+    answer = f'{simplified_solution[0]}/{simplified_solution[1]}'
+
+    config.write_solution(question,answer)
+
+# TODO: Add in division function for this same process
+# TODO: This function is going to be added back in when I split out the TUI event loop into two events, the way it's handled right now doesn't work.
+def multiply_remainders() -> bool:
+    '''
+    This generates a question based off the remainders of numbers when divided by a given number.
+    '''
+    numerator1 = randint(2,20)
+    numerator2 = randint(2,20)
+    denominator = randint(2,9)
+
+    remainder1 = numerator1 % denominator
+    remainder2 = numerator2 % denominator
+
+    answer = (remainder1 * remainder2) % denominator
+
+    question = f'When integer a is divided by {denominator}, the remainder is {remainder1}.\nWhen integer b is divided by {denominator}, the remainder is {remainder2}.\nWhat is the remainder when a x b is divided by {denominator}?'
+
+    config.write_solution(question,answer)
---
+++ math_functions/function_dicts.py
@@ -0,0 +1,23 @@
+from math_functions.foundations import basic_factors, basic_fractions, basic_multiples
+# This is going to be the holding area for all of my function dicts so I don't end up with an insane
+# import statement in my TUI file.
+
+foundations_dict = {
+    'multiply by fractions': basic_fractions.multiply_fractions_quiz,
+    'divide by fractions': basic_fractions.divide_fractions_quiz,
+    'multiply fractions with exponents': basic_fractions.multiply_fractions_with_exponents,
+    # This is going to get added back in after I split the TUI event loop into two
+    # 'multiply remainders': basic_fractions.multiply_remainders,
+    'n-digit multiples': basic_multiples.n_digit_multiples_quiz,
+    'common n-digit multiples': basic_multiples.common_n_digit_multiples_quiz,
+    'factor operations': basic_factors.factor_quiz,
+    'prime factor operations': basic_factors.prime_factor_quiz,
+}
+
+category_dict = {
+    'Foundations': foundations_dict
+}
+
+# This is just to test my imports
+if __name__ == "__main__":
+    basic_fractions.multiply_fractions_quiz()
