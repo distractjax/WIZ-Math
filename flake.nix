1270139b9bebf56db060eb912379502fcf6186ff
distractjax <distractjax@gmail.com>
2026-02-26 15:30:47 -0500

    Added pandas to my Nix flake.

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
           packages = with pkgs; [
             (python3.withPackages (ps: with ps; [
               debugpy
+              pandas
             ]))
             basedpyright
             ruff
--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,4 @@
-from os import path
-
 def test():
-    print(f"{path.expanduser('~')}")
 
 if __name__ == '__main__':
     test()
