6af5bfdbb7e2f4734a0a139c4ec81531375f816b
distractjax <distractjax@gmail.com>
2026-02-27 00:44:47 -0500

    Moved the df.replace() function to the initial load instead of every module.

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -6,6 +6,7 @@ from sqlite3 import connect
 def table_to_df(tablename: str = "problem_history", filepath: str = config.SQLITE_PATH):
     with connect(filepath) as conn:
         df = pd.read_sql(f"SELECT * FROM {tablename}",conn)
+    df.replace(0,np.nan,inplace=True)
     return df
 
 # Overview Window
@@ -14,7 +15,6 @@ def table_to_df(tablename: str = "problem_history", filepath: str = config.SQLIT
 
 def create_overview():
     df = table_to_df()
-    df.replace(0,np.nan,inplace=True)
     df = df.groupby("q_type").count()
     df['was_wrong'] = df['exec_time'] - df['was_right']
     for x in [x for x in df.columns if x != 'was_wrong' and x != 'was_right']:
@@ -37,7 +37,6 @@ def create_module_view(module: str = "Foundations"):
 
 def create_history_view():
     df = table_to_df()
-    df.replace(0,np.nan,inplace=True)
     df['exec_time'] = pd.to_datetime(df['exec_time'],format='%Y-%m-%d %H:%M:%S.%f')
     df['exec_time'] = df['exec_time'].dt.to_period("D")
     df = df.groupby('exec_time').count()
@@ -52,6 +51,7 @@ def create_history_view():
 
 def create_rank_view():
     df = table_to_df()
+    df.replace(0,np.nan,inplace=True)
     pass
 
 if __name__ == '__main__':
