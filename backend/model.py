907cb14cc11fad0b167ef219c4ab0128d36ef172
distractjax <distractjax@gmail.com>
2026-03-19 13:25:12 -0400

    Implemented Model and Update portions of the backend architecture.

--- backend/model.py
+++ backend/model.py
@@ -11,14 +11,69 @@ if TYPE_CHECKING:
 # Step-by-step, what is my backend doing?
 # 1. It's sitting idle and waiting for a request. (IDLE)
 # 2. It receives a message to generate a new question and it then generates the question-answer pair. (NEW QUESTION)
-# 3. It writes the question metadata to a database. (LOG QUESTION)
-# 4. It sends a JSON object to the frontend containing the question and the answer. (PUSH QUESTION)
 # 5. It waits to receive the answer status and completion time from the frontend. (This can re-use the IDLE state).
 # 6. It writes the answer status and completion time to the database. (LOG ANSWER)
 # 7. It returns to IDLE.
 # 8. It queries the database and converts that to a dataframe, manipulates data to get the right shape. (GET HISTORY)
     # 8a. I do think this qualifies as one event, even though there are multiple sub-parts.
-# 9. It converts the dataframe to a JSON object and pushes it to the frontend (PUSH HISTORY)
+
+# Okay, let's re-think this. Don't think of every event, just think: when does the backend change?
+# 1. It's sitting idle
+# 2. The user inputs a command to generate a new question. (GENERATE QUESTION)
+# 3. A command runs and generates that question, returning the question and answer (RETRIEVE QUESTION)
+# 4. A command runs and logs that question to the database
+
+class Cmd(Enum):
+    GENERATE_QUESTION = auto()
+    SAVE_A_TO_DB = auto()
+    SAVE_Q_TO_DB = auto()
+    CHECK_ANSWER = auto()
+    PULL_STATS = auto()
+    NONE = auto()
+
+# Message classes
+class Msg(Enum):
+    # End the program
+    QUIT = auto()
+    STATS_REQUESTED = auto()
+
+# Status classes
+class AppStatus(Enum):
+    IDLE = auto()
+    STATS_REQUESTED = auto()
+    STATS_PULLED = auto()
+    GENERATING_QUESTION = auto()
+    GENERATED_QUESTION = auto()
+    ANSWER_SUBMITTED = auto()
+    ANSWER_CHECKED = auto()
+
+# NEW QUESTION state
+@dataclass(frozen=True)
+class NewQuestionRequested:
+    # Derived from side-menu
+    question_type: str
+    # Derived from header
+    question_module: str
+
+@dataclass(frozen=True)
+class NewQuestionGenerated:
+    question: str
+    answer: str
+
+# RETRIVE ANSWER
+@dataclass(frozen=True)
+class AnswerSubmitted:
+    user_answer: str
+    end_time: datetime
+
+@dataclass(frozen=True)
+class AnswerChecked:
+    is_answer_correct: bool
+
+# GET HISTORY state
+@dataclass(frozen=True)
+class StatsLoaded:
+    df: "pd.DataFrame"
 
 # State classes
 @dataclass(frozen=True)
@@ -27,10 +82,10 @@ class MathState:
     question_type: str = ""
     question_module: str = ""
     answer: str = ""
+    user_answer: str = ""
     is_answer_correct: bool = False
-    user_input: str = ""
-    start_time: datetime = datetime.now()
-    question_time: float = 0.0
+    start_time: datetime = field(default_factory = datetime.now)
+    end_time: datetime = field(default_factory = datetime.now)
 
 @dataclass(frozen=True)
 class GlobalState:
@@ -47,47 +102,5 @@ class GlobalState:
 
     # Loop components
     is_running: bool = True
-    state: str = "IDLE"
+    state: AppStatus = AppStatus.IDLE
 
-# Message classes
-class Msg(Enum):
-    # End the program
-    QUIT = auto()
-
-# NEW QUESTION state
-@dataclass(frozen=True)
-class RequestNewQuestion:
-    # Derived from side-menu
-    question_type: str
-    # Derived from header
-    question_module: str
-
-# LOG QUESTION state
-@dataclass(frozen=True)
-class RequestLogQuestion:
-    question_type: str
-    question_module: str
-
-# PUSH QUESTION state
-@dataclass(frozen=True)
-class RequestPushQuestion:
-    question_type: str
-    question_module: str
-
-# LOG ANSWER state
-@dataclass(frozen=True)
-class RequestLogAnswer:
-    question_type: str
-    question_module: str
-
-# GET HISTORY state
-@dataclass(frozen=True)
-class RequestGetHistory:
-    question_type: str
-    question_module: str
-
-# PUSH HISTORY state
-@dataclass(frozen=True)
-class RequestPushHistory:
-    question_type: str
-    question_module: str
--- backend/update.py
+++ backend/update.py
@@ -2,21 +2,44 @@ from typing import Union
 import model as m
 from dataclasses import replace
 import function_dicts
+from datetime import datetime
 
-Message = Union[m.Msg, m.RequestNewQuestion]
+Message = Union[
+    m.Msg, 
+    m.NewQuestionRequested,
+    m.NewQuestionGenerated,
+    m.AnswerSubmitted,
+    m.AnswerChecked,
+    m.StatsLoaded,
+]
 
-def update(model: m.GlobalState, message: Message):
+# update just updates the model's state
+def update(model: m.GlobalState, message: Message) -> tuple[m.GlobalState, m.Cmd]:
     match message:
         case m.Msg.QUIT:
-            return replace(model, is_running = False)
-        case m.RequestNewQuestion(question_module, question_type):
-            # Fill in this logic when you're less sleepy.
-            function_dicts.category_dict[question_module][question_type]()
-            return replace(model, math = 
-                replace(model.math, 
-                        question_module = question_module,
-                        question_type = question_type,
-                        )
-            )
+            return replace(model, is_running = False), m.Cmd.NONE
+
+        case m.NewQuestionRequested(q_type, q_module):
+            return replace(model, state = m.AppStatus.GENERATING_QUESTION,
+                           math = replace(model.math, question_type = q_type, question_module = q_module)), m.Cmd.GENERATE_QUESTION
+
+        case m.NewQuestionGenerated(q, a):
+            return replace(model, state = m.AppStatus.GENERATED_QUESTION,
+                           math = replace(model.math, question = q, answer = a)), m.Cmd.SAVE_Q_TO_DB
+
+        case m.AnswerSubmitted(u_answer, end_time):
+            return replace(model, state = m.AppStatus.ANSWER_SUBMITTED,
+                           math = replace(model.math, user_answer = u_answer, end_time = end_time)), m.Cmd.CHECK_ANSWER
+
+        case m.AnswerChecked(is_correct):
+            return replace(model, state = m.AppStatus.ANSWER_CHECKED,
+                           math = replace(model.math, is_answer_correct = is_correct)), m.Cmd.SAVE_A_TO_DB
+
+        case m.Msg.STATS_REQUESTED:
+            return replace(model, state = m.AppStatus.STATS_REQUESTED), m.Cmd.PULL_STATS
+
+        case m.StatsLoaded(df): 
+            return replace(model, state = m.AppStatus.STATS_PULLED, problem_history = df), m.Cmd.NONE
+
         case _:
-            return model
+            return model, m.Cmd.NONE
